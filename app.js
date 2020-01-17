1d2a02813a3d6fab85941bb61dc62610c20e431f
TurtleWolfe <JonPohlner@gmail.com>
2020-01-17 17:52:24 -0500

    second assignment, 2 paths slash and users

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 const express = require('express');
 
 const app = express();
-// const routes = require('./routes');
-// 1 server (port3000)
-// 2 / & /users
+//runs with every request
 app.use((requ, resp, next) => {
     console.log('always run MiddleWare');
-    // resp.send('<h1>MiddleWare</h1>');
     next();
 });
-
+//runs on user request only
 app.use('/users', (requ, resp, next) => {
     console.log('only runs with users');
-    // resp.send('<h1>MiddleWare</h1>');
     next();
 });
-
+//runs on user request only
 app.use('/users', (requ, resp, next) => {
     console.log('users');
     resp.send('<h1>users</h1>');
 });
-
+//runs on slash request only
 app.use('/', (requ, resp, next) => {
     console.log('only runs with slash');
-    // resp.send('<h1>MiddleWare</h1>');
     next();
 });
-
+//runs on slash request only
 app.use('/', (requ, resp, next) => {
     console.log('slash');
     resp.send('<h1>slash</h1>');
