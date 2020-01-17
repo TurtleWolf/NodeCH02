3a531ba40ede503dd7cb65d8cf437b9a4ba0f278
TurtleWolfe <JonPohlner@gmail.com>
2020-01-17 15:31:27 -0500

    express app; next

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 
-const app = express;
+const app = express();
 // const routes = require('./routes');
 // 1 server (port3000)
-const server = http.createServer(app);
-server.listen(3000);
+app.use((requ, resp, next) => {
+    console.log('MiddleWare');
+    // resp.send('<h1>MiddleWare</h1>');
+    next();
+});
+app.use((requ, resp, next) => {
+    console.log('MidWife');
+    resp.send('<h1>WifeMid</h1>');
+});
+app.listen(3000);
